25356d548007e6f19cf2f92d17457b987b51d80f
Aaron Malone <aaronmalone@gmail.com>
2015-06-26 01:35:42 -0400

    First pass at towers of hanoi algorithm

--- hw1/HW01.hs
+++ hw1/HW01.hs
@@ -48,4 +48,6 @@ type Peg = String
 type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi = undefined
+hanoi 0 _ _ _ = []
+hanoi 1 a _ c = [(a, c)]
+hanoi n a b c = (hanoi (n-1) a c b) ++ [(a, c)] ++ hanoi (n-1) b a c
