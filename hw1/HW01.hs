a5d7ddfd5da9c4635aa2e6d1ead4c62a54895e0b
Aaron Malone <aaronmalone@gmail.com>
2015-06-26 01:37:43 -0400

    Removed unnecessary parens from hanoi implementation

--- hw1/HW01.hs
+++ hw1/HW01.hs
@@ -50,4 +50,4 @@ type Move = (Peg, Peg)
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 hanoi 0 _ _ _ = []
 hanoi 1 a _ c = [(a, c)]
-hanoi n a b c = (hanoi (n-1) a c b) ++ [(a, c)] ++ hanoi (n-1) b a c
+hanoi n a b c = hanoi (n-1) a c b ++ [(a, c)] ++ hanoi (n-1) b a c
